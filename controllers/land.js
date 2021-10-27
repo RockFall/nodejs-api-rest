3711382933f46a4165672813c038a0b329ae5fd2
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2021-10-27 15:25:38 -0300

    Land GET byID/all, UPDATE, DELETE

--- controllers/land.js
+++ controllers/land.js
@@ -2,11 +2,30 @@ const Lands = require('../models/land')
 
 module.exports = app => {
   app.get('/terrenos', (req, res) => {
-    Lands.list(res)
+    Lands.getAll(res)
+  })
+
+  app.get('/terrenos/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    Lands.getById(id, res)
   })
 
   app.post('/terrenos', (req, res) => {
     const land = req.body
     Lands.add(land, res)
+  })
+
+  app.patch('/terrenos/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const values = req.body
+
+    Lands.update(id, values, res)
+  })
+
+  app.delete('/terrenos/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    Lands.delete(id, res)
   })
 }
--- models/land.js
+++ models/land.js
@@ -2,60 +2,115 @@ const connection = require('../infrastructure/connection')
 const moment = require('moment')
 
 class Lands {
+
+  // Adds a new Land into the database
   add(land, res) {
-    // Pré-processamento das datas
+
+    // Pre-processing of dates
     const soldDate = moment(land['soldDate'], 'DD/MM/YYYY').format('YYYY-MM-DD')
     const builtDate = moment(land['builtDate'], 'DD/MM/YYYY').format('YYYY-MM-DD')
-    // Armazenamento das datas de criação e modificação
+
     const creationDate = moment().format('YYYY-MM-DD HH:MM:SS')
     const lastModifiedDate = moment().format('YYYY-MM-DD HH:mm:ss')
     land['soldDate'] = soldDate
     land['builtDate'] = builtDate
-    // Variável final após processamento
+
+    // Final variable
     const datedLand = {...land, creationDate, lastModifiedDate}
 
-    // Check se os valores são válidos
+    // Check if inputs are valid
     const isValidSoldDate = moment(soldDate).isSameOrBefore(creationDate)
     const isValidBuiltDate = moment(builtDate).isSameOrBefore(creationDate)
-    // Mensagens de erro
     const validations = [
       {
         name: 'soldDate',
         valid: isValidSoldDate,
-        message: 'Must be less or equal current datetime'
+        message: 'Must be less or equal current datetime',
+        soldDate: soldDate,
+        creationDate: creationDate
       },
       {
         name: 'builtDate',
         valid: isValidBuiltDate,
-        message: 'Must be less or equal current datetime'
+        message: 'Must be less or equal current datetime',
+        soldDate: builtDate,
+        creationDate: creationDate
       }
     ]
-    // Caso exista erros, exibe-os e retorna
+
+    // If any errors, returns them
     const errors = validations.filter(field => !field.valid)
     if (errors.length > 0) {
       res.status(400).json(errors)
       return
     }
 
+    // Insertion query
     const sql = 'INSERT INTO Land SET ?'
-
-    connection.query(sql, datedLand, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro)
+    connection.query(sql, datedLand, (err, results) => {
+      if (err) {
+        res.status(400).json(err)
       } else {
-        res.status(201).json(resultados)
+        res.status(201).json(datedLand)
       }
     })
   }
 
-  list(res) {
+  // Returns a list of all lands
+  getAll(res) {
     const sql = 'SELECT * FROM land'
 
-    connection.query(sql, (err, result) => {
+    connection.query(sql, (err, results) => {
       if (err)
         res.status(400).json()
       else
-        res.status(200).json(result)
+        res.status(200).json(results)
+    })
+  }
+
+  // Return a land based on given ID
+  getById(id, res) {
+    const sql = `SELECT * FROM land WHERE idLand =${id}`
+
+    connection.query(sql, (err, results) => {
+      const land = results[0]
+      if (err)
+        res.status(400).json(err)
+      else
+        res.status(200).json(land)
+    })
+  }
+
+  // Updates data from land
+  update(id, values, res) {
+    if (values.soldDate) {
+      values.soldDate = moment(values.soldDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
+    } if (values.builtDate) {
+      values.builtDate = moment(values.builtDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
+    }
+
+    const lastModifiedDate = moment().format('YYYY-MM-DD HH:mm:ss')
+    values = {...values, lastModifiedDate}
+
+    const sql = `UPDATE land SET ? WHERE idLand=?`
+
+    connection.query(sql, [values, id], (err, results) => {
+      if (err)
+        res.status(400).json(err)
+      else
+        res.status(200).json({...values, id})
+    })
+  }
+
+  // Deletes data with given id
+  delete(id, res) {
+    const sql = `DELETE FROM land WHERE idLand=?`
+
+    connection.query(sql, id, (err, results) => {
+      if (err)
+        res.status(400).json(err)
+      else
+        res.status(200).json({id})
     })
   }
 }
